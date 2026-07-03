2ee6229fc9031ef093fa55ecb676a8226cd93c1c
Svein <sklonteig@gmail.com>
2026-07-03 12:17:44 +0200

    Initial Commit

---
+++ Real Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Real Weather
+//
+//  Created by SveinKlonteig on 03/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Real Weather/Real_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_WeatherApp.swift
+//  Real Weather
+//
+//  Created by SveinKlonteig on 03/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Real_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
